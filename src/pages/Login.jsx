17b4a064b12c4ec18d9997d512df817da6b7ae25
danciueduard <danciu.euu@gmail.com>
2023-11-27 14:40:51 +0100

    end of the project

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,8 @@
 import styled from "styled-components";
+import LoginForm from "../features/authentication/LoginForm";
+import Logo from "../ui/Logo";
+import Heading from "../ui/Heading";
+import CabinTable from "../features/cabins/CabinTable";
 
 const LoginLayout = styled.main`
   min-height: 100vh;
@@ -12,7 +16,13 @@ const LoginLayout = styled.main`
 `;
 
 function Login() {
-  return <LoginLayout>Login</LoginLayout>;
+  return (
+    <LoginLayout>
+      <Logo />
+      <Heading as="h4">Log in to your account</Heading>
+      <LoginForm />
+    </LoginLayout>
+  );
 }
 
 export default Login;
